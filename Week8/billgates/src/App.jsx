423fcca6d809ed38bef423e9ac3cd86a5f66ca58
mffedai1 <mfatihfedai1@gmail.com>
2024-05-08 20:08:20 +0300

    bill gates initialized

---
+++ Week8/billgates/src/App.jsx
@@ -0,0 +1,18 @@
+//import { useState } from 'react'
+import './App.css'
+import Header from './Header'
+import Hero from './Hero'
+
+
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <Hero/>
+    </>
+  )
+}
+
+export default App

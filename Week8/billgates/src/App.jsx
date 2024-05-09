d7e975c3fe1535f6cc4309c195b737e147edfcf9
mffedai1 <mfatihfedai1@gmail.com>
2024-05-09 18:43:57 +0300

    bill gates completed

--- Week8/billgates/src/App.jsx
+++ Week8/billgates/src/App.jsx
@@ -1,74 +1,88 @@
-import './App.css'
-import { useState } from "react"
-import data from './data.json'
+import "./App.css";
+import { useState } from "react";
+import data from "./data.json";
 
 function App() {
+  const [totalMoney, setTotalMoney] = useState(100000000000);
+  const [itemInputs, setItemInputs] = useState(Array(data.length).fill(0));
 
-  const [totalMoney, setTotalMoney] = useState(100000000000)
-  const [itemInput, setItemInput] = useState(0)
-  
-  function sellFunc(){
-    if(itemInput > 0){
-          setItemInput(itemInput - 1)
-         }
-    inputFunc    
-  }
-
-  function buyFunc(){
-    
-    inputFunc
-  }
-
-  function inputFunc(){
-    
-    let price = data[1].price
-    //setTotalMoney(totalMoney - (price*itemInput))
-    setTotalMoney(price)
+  const sellFunc = (index, heroPrice) => {
+    if (itemInputs[index] > 0) {
+      setItemInputs((prevItemInputs) => {
+        const newInputs = [...prevItemInputs];
+        newInputs[index] -= 1;
+        return newInputs;
+      });
+      setTotalMoney((prevPrice) => prevPrice + heroPrice);
+    }
   };
 
-  const buyFunct = (hero) => {
-    setItemInput(prevItemInput => itemInput + 1)
-    setTotalMoney(prevPrice => prevPrice - hero.price);
+  const buyFunc = (index, heroPrice) => {
+    setItemInputs((prevItemInputs) => {
+      const newInputs = [...prevItemInputs];
+      newInputs[index] += 1;
+      return newInputs;
+    });
+    setTotalMoney((prevPrice) => prevPrice - heroPrice);
   };
 
   return (
-    
     <>
-    
-    <div className="headerContainer">
+      <div className="headerContainer">
         <img src="./img/billgates.jpg" alt="billgates" />
         <h1>Spend Bill Gates' Money</h1>
-    </div>
-    
-    <div className="navbar">
-    {totalMoney}
-    </div>
-
-    <div className='container'>
-        {data.map((hero)=>(
-            <div className='heroContainer' key={hero.id}>
-                
-                <img src={hero.img} alt="" />
-                <h3>{hero.name}</h3>
-                <div><span className='heroPrice'>${hero.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}</span></div>
-                <div className='heroButtonContainer'>
-                    <button className='btnHeroSell'onClick={sellFunc}>Sell</button>
-                    <input value={itemInput} onChange={(e) => handleInputChange(hero.id, e.target.value)} type="number" />
-                    <button className='btnHeroBuy' onClick={() => buyFunct(hero)}>Buy</button>  
-                </div>
-            </div>  
-        ))}
-        
-        
       </div>
 
+      <div className="navbar">
+        $ {totalMoney.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+      </div>
 
-
-
-
+      <div className="container">
+        {data.map((hero) => (
+          <div className="heroContainer" key={hero.id}>
+            <img src={hero.img} alt="" />
+            <h3>{hero.name}</h3>
+            <div>
+              <span className="heroPrice">
+                ${hero.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+              </span>
+            </div>
+            <div className="heroButtonContainer">
+              <button className="btnHeroSell" onClick={() => sellFunc(hero.id, hero.price)}> Sell </button>
+              <input type="number" value={itemInputs[hero.id]} onChange={() => {}}/>
+              <button className="btnHeroBuy" onClick={() => buyFunc(hero.id, hero.price)}>Buy </button>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="footer-receipt" style={{ display: itemInputs.some(quantity => quantity > 0) ? 'flex' : 'none' }}>
+        <div className="receipt-container">
+          <h1>Your Receipt</h1>
+          {data.map((hero) =>
+              itemInputs[hero.id] > 0 && (
+                <div className="receipt" key={hero.id}>
+                <h3>{hero.name}</h3>
+                <h3>x{itemInputs[hero.id]}</h3>
+                <div className="price">${hero.price * itemInputs[hero.id] % 1000000 === 0 ? 
+                  `${((hero.price * itemInputs[hero.id]) / 1000000).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}b` : 
+                  (hero.price * itemInputs[hero.id]) % 1000 === 0 ? 
+                  `${((hero.price * itemInputs[hero.id]) / 1000).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}m` : 
+                  hero.price * itemInputs[hero.id]
+                }</div>
+              </div>
+              )
+          )}
+          <hr />
+          <div className="receipt">
+            <h3>TOTAL</h3>
+            <div className="price">
+              ${(100000000000 - totalMoney).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+            </div>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-
-export default App
+export default App;

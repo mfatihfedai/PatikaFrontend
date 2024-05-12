eb2ae001273189c496a3231068216efaa07eab8c
mffedai1 <mfatihfedai1@gmail.com>
2024-05-12 23:22:34 +0300

    bulmaca button css is added

--- Week10/bulmaca/oyun/src/App.js
+++ Week10/bulmaca/oyun/src/App.js
@@ -1,23 +1,79 @@
 import './App.css';
 import { useState , useEffect } from 'react';
 import { DATA } from './data';
+const alphabet = ["A", "B", "C", "Ç", "D", "E", "F", "G", "Ğ", "H", "I", "İ", "J", "K", "L", "M", "N", "O", "Ö", "P", "R", "S", "Ş", "T", "U", "Ü", "V", "Y", "Z"];
+
 
 function App() {
   const [index, setIndex] = useState(0);
   const [question, setQuestion] = useState("");
   const [answer, setAnswer] = useState("");
+  const [answerArray, setAnswerArray] = useState([]);
+  const [keywords, setKeywords] = useState([]);
+  const [resultQuestion, setResultQuestion] = useState(false);
+
+
+  const shuffle = (array) => {
+    return array.sort(() => Math.random() - 0.5);
+  } 
+
+  const setKeyword = (keyword) => {
+    if(keywords.length < answer.length){
+      keywords.push(keyword);
+      setKeywords([...keywords]);
+    }
+    if(keywords.length == answer.length){
+      if(answer == keywords.join("")){
+        setIndex(index + 1);
+        setKeywords([]);
+        setResultQuestion(true);
+      } else {
+        alert('cevap yanlış')
+      }
+    }
+
+  }
 
 
   useEffect(() => {
-    setAnswer(DATA[index].answer);
+    setResultQuestion(false);
+    const answer = DATA[index].answer.toLowerCase();
+    setAnswer(answer);
     setQuestion(DATA[index].question);
-    
-  }, []);
+    const stringToArray = answer.split("");
+    stringToArray.push(alphabet[Math.floor(Math.random() * alphabet.length)]);
+    stringToArray.push(alphabet[Math.floor(Math.random() * alphabet.length)]);
+    stringToArray.push(alphabet[Math.floor(Math.random() * alphabet.length)]);
+    const alphabetLowerData = stringToArray.map((answer) => answer.toLowerCase());
+    setAnswerArray(shuffle(alphabetLowerData));
+
+  }, [resultQuestion]);
+
+  const removeKeyword = (index) => {
+    keywords.splice(index, 1);
+    setKeywords([...keywords]);
+  } 
 
   return (
     <div className="App">
       <div>
         <span>{question}</span>
+      </div>
+
+      <div>
+        {keywords.map((item, index) => (
+          <span onClick={() => removeKeyword(index)} key={index}>
+            {item}
+          </span>
+        ))}
+      </div>
+
+      <div>
+        {answerArray.map((item,index) => (
+          <button key={index} onClick={() => setKeyword(item)}>
+            {item}
+          </button>
+         ))}
       </div>
     </div>
   );
--- Week10/bulmaca/oyun/src/data.js
+++ Week10/bulmaca/oyun/src/data.js
@@ -1,3 +1,5 @@
 export const DATA = [
-    {question: 'Bir Hayvan adı ?', answer: 'Essek'}
+    {question: 'Bir Hayvan Adı ?', answer: 'Essek'},
+    {question: 'Bir Balık Adı ?', answer: 'Kefal'},
+    {question: 'Bir Renk Adı ?', answer: 'Mavi'}
 ]

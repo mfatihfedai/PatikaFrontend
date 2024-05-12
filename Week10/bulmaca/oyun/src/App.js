6bea184f3affa55ac466f5b0297262a8c534a5e1
mffedai1 <mfatihfedai1@gmail.com>
2024-05-12 23:57:11 +0300

    bulmaca is finished

--- Week10/bulmaca/oyun/src/App.js
+++ Week10/bulmaca/oyun/src/App.js
@@ -3,7 +3,6 @@ import { useState , useEffect } from 'react';
 import { DATA } from './data';
 const alphabet = ["A", "B", "C", "Ç", "D", "E", "F", "G", "Ğ", "H", "I", "İ", "J", "K", "L", "M", "N", "O", "Ö", "P", "R", "S", "Ş", "T", "U", "Ü", "V", "Y", "Z"];
 
-
 function App() {
   const [index, setIndex] = useState(0);
   const [question, setQuestion] = useState("");
@@ -11,12 +10,11 @@ function App() {
   const [answerArray, setAnswerArray] = useState([]);
   const [keywords, setKeywords] = useState([]);
   const [resultQuestion, setResultQuestion] = useState(false);
-
+  const [wrong, setWrong] = useState(true);
 
   const shuffle = (array) => {
     return array.sort(() => Math.random() - 0.5);
   } 
-
   const setKeyword = (keyword) => {
     if(keywords.length < answer.length){
       keywords.push(keyword);
@@ -28,25 +26,25 @@ function App() {
         setKeywords([]);
         setResultQuestion(true);
       } else {
-        alert('cevap yanlış')
+        setWrong(true);
       }
     }
-
   }
-
-
   useEffect(() => {
+    setWrong(false);
     setResultQuestion(false);
-    const answer = DATA[index].answer.toLowerCase();
-    setAnswer(answer);
-    setQuestion(DATA[index].question);
-    const stringToArray = answer.split("");
-    stringToArray.push(alphabet[Math.floor(Math.random() * alphabet.length)]);
-    stringToArray.push(alphabet[Math.floor(Math.random() * alphabet.length)]);
-    stringToArray.push(alphabet[Math.floor(Math.random() * alphabet.length)]);
-    const alphabetLowerData = stringToArray.map((answer) => answer.toLowerCase());
-    setAnswerArray(shuffle(alphabetLowerData));
-
+    setAnswer("");
+    if(typeof DATA[index] != "undefined"){
+      const answer = DATA[index].answer.toLowerCase();
+      setAnswer(answer);
+      setQuestion(DATA[index].question);
+      const stringToArray = answer.split("");
+      stringToArray.push(alphabet[Math.floor(Math.random() * alphabet.length)]);
+      stringToArray.push(alphabet[Math.floor(Math.random() * alphabet.length)]);
+      stringToArray.push(alphabet[Math.floor(Math.random() * alphabet.length)]);
+      const alphabetLowerData = stringToArray.map((answer) => answer.toLowerCase());
+      setAnswerArray(shuffle(alphabetLowerData));
+    }
   }, [resultQuestion]);
 
   const removeKeyword = (index) => {
@@ -56,27 +54,33 @@ function App() {
 
   return (
     <div className="App">
+      {answer != "" && 
       <div>
-        <span>{question}</span>
+        <div>
+          <span className='question-name'>{question}</span>
+        </div>
+        <div className='question-area'>
+          {keywords.map((item, index) => (
+            <span style={{'border-bottom':(wrong) ? '3px solid red' : '3px solid #ddd'}} className='question' onClick={() => removeKeyword(index)} key={index}>
+              {item}
+            </span>
+          ))}
+        </div>
+        <div className='button-area'>
+          {answerArray.map((item,index) => (
+            <button key={index} onClick={() => setKeyword(item)}>
+              {item}
+            </button>
+          ))}
+        </div>
       </div>
-
-      <div>
-        {keywords.map((item, index) => (
-          <span onClick={() => removeKeyword(index)} key={index}>
-            {item}
-          </span>
-        ))}
-      </div>
-
-      <div>
-        {answerArray.map((item,index) => (
-          <button key={index} onClick={() => setKeyword(item)}>
-            {item}
-          </button>
-         ))}
-      </div>
+      }
+      {answer == "" &&
+        <div className='empty-message'>
+          Sorular Bitti !
+        </div>
+      }
     </div>
   );
 }
-
 export default App;
